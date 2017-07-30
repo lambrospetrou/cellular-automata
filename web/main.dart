1942c7ea9e0e86c7d3fad17d232af0d05654e934
Lambros Petrou <lambrospetrou@gmail.com>
2017-07-30 14:28:23 +0100

    Fixed width,height due to scrollbars. Also using window.animationFrame now instead of timer to allow maximum refresh rate.

--- web/main.dart
+++ web/main.dart
@@ -5,59 +5,79 @@ import 'dart:async';
 import 'dart:html' as html;
 import 'package:stagexl/stagexl.dart' as sxl;
 
-const STAGE_WIDTH = 1280;
-const STAGE_HEIGHT = 800;
-
 void main() {
+  final WINDOW_WIDTH = html.window.innerWidth;
+  final WINDOW_HEIGHT = html.window.innerHeight;
+  print('Window width $WINDOW_WIDTH and height $WINDOW_HEIGHT');
 
   sxl.StageOptions options = new sxl.StageOptions()
-    ..backgroundColor = sxl.Color.WhiteSmoke
+    ..backgroundColor = sxl.Color.Tomato
     ..antialias = true
     ..renderEngine = sxl.RenderEngine.WebGL;
 
   var canvas = html.querySelector('#stage');
-  var stage = new sxl.Stage(canvas, width: STAGE_WIDTH, height: STAGE_HEIGHT, options: options);
+  var stage = new sxl.Stage(canvas,
+      height: WINDOW_HEIGHT, width: WINDOW_WIDTH, options: options);
   var renderLoop = new sxl.RenderLoop();
   renderLoop.addStage(stage);
 
-  Painter painter = new Painter(stage);
+  CellularPainter painter = new CellularPainter(stage);
   html.querySelector('#tool-r').onClick.listen((evt) {
     if (painter != null) {
       painter.stopAndClear();
-      painter = new Painter(stage);
+      painter = new CellularPainter(stage);
     }
   });
 }
 
-class Painter {
-  final sxl.Stage stage;
-  Timer timer;
+class CellularPainter {
+  final sxl.Stage _stage;
+  sxl.BitmapDataUpdateBatch _canvasBitmapDataBuffer;
 
-  Painter(this.stage) {
-    sxl.BitmapData canvasBitmapData = new sxl.BitmapData(STAGE_WIDTH, STAGE_HEIGHT);
-    sxl.BitmapDataUpdateBatch canvasBitmapDataBuffer = new sxl.BitmapDataUpdateBatch(canvasBitmapData);
+  int _STAGE_WIDTH, _STAGE_HEIGHT;
+
+  bool _isStopped = false;
+
+  CellularPainter(this._stage) {
+    _STAGE_WIDTH = this._stage.stageWidth;
+    _STAGE_HEIGHT = this._stage.stageHeight;
+
+    print('Stage width $_STAGE_WIDTH and height $_STAGE_HEIGHT');
+
+    sxl.BitmapData canvasBitmapData =
+        new sxl.BitmapData(_STAGE_WIDTH, _STAGE_HEIGHT);
+    _canvasBitmapDataBuffer = new sxl.BitmapDataUpdateBatch(canvasBitmapData);
     sxl.Bitmap drawingCache = new sxl.Bitmap(canvasBitmapData);
-    drawingCache.addTo(stage);
+    drawingCache.addTo(_stage);
+    run();
+  }
 
-    const TIMEOUT = const Duration(milliseconds: 33);
+  int _x = 0;
 
-    print('starting timer');
-    int x = 0;
-    timer = new Timer.periodic(TIMEOUT, (Timer timer) {
-      if (x>STAGE_WIDTH) {
-        timer.cancel();
-        print('finished timer!');
-        return;
-      }
-      for (var end=x+10; x<end; x++) {
-        canvasBitmapDataBuffer.setPixel32(x, 10, sxl.Color.CadetBlue);
-      }
-      canvasBitmapDataBuffer.update();
-    });
+  void _tickUpdate(num delta) {
+    print('Tick: $delta');
+    if (_isStopped) return;
+    // TICK UPDATE START
+
+    if (_x > _STAGE_WIDTH) {
+      print('finished!');
+      return;
+    }
+    for (var end = _x + 10; _x < end; _x++) {
+      _canvasBitmapDataBuffer.setPixel32(_x, 10, sxl.Color.CadetBlue);
+    }
+    _canvasBitmapDataBuffer.update();
+
+    // TICK UPDATE END
+    run();
+  }
+
+  void run() {
+    html.window.animationFrame.then(_tickUpdate);
   }
 
   void stopAndClear() {
-    timer.cancel();
-    stage.removeChildren();
+    _isStopped = true;
+    _stage.removeChildren();
   }
 }
